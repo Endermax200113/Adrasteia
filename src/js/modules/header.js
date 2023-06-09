023dd734ffe6ef1cd2e42fb78eb2b9aca7a163bc
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2023-06-09 18:13:31 +0300

    Added and adapted 'Main' on the site

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,16 +1,11 @@
 import $ from 'jquery';
 
 export function toggleMenu() {
-	console.log(1);
-
 	$(window).on('load', () => {
-		console.log(2);
-
 		let btnMenu = $('#header__menu-button');
 		let listMenu = $('#header__list');
 
 		btnMenu.on('click', () => {
-			console.log(3);
 			btnMenu.toggleClass('header__menu-button--disabled');
 			btnMenu.toggleClass('header__menu-button--enabled');
 			listMenu.toggleClass('header__list--show');
